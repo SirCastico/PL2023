b25d8d5857cd635555b41a56c28b95c44f9cea07
sircastico <carlosemanuelslb2@gmail.com>
2023-02-24 18:44:30 +0000

    TPC2

---
+++ TPC2/tpc2.py
@@ -0,0 +1,59 @@
+import sys
+
+class StringAggregateCompare:
+    string: str
+    comp: str
+    i: int
+
+    def __init__(self, string:str) -> None:
+        self.string = string.lower()
+        self.comp = ""
+        self.i = 0
+
+    def addAndCompare(self, char) -> bool:
+        if(self.i==len(self.string)):
+            return True
+        
+        char = char.lower()
+        if(self.string[self.i]==char):
+            self.i += 1
+            self.comp += char
+            return self.comp==self.string
+        else:
+            self.i = 0
+            self.comp = ""
+            return False
+
+
+class Somador:
+    soma: int = 0
+    is_on: bool = True
+
+    def sum_line(self, line:str):
+        on_cmp = StringAggregateCompare("On")
+        off_cmp = StringAggregateCompare("Off")
+        cur_digit_str: str = ""
+
+        for c in line:
+            if(on_cmp.addAndCompare(c)):
+                self.is_on = True
+            elif(off_cmp.addAndCompare(c)):
+                self.is_on = False
+            elif(c=="="):
+                print(self.soma)
+            
+            if(not self.is_on):
+                continue
+            
+            if(c.isdigit()):
+                cur_digit_str += c
+            elif(cur_digit_str != ""):
+                self.soma += int(cur_digit_str)
+                cur_digit_str = ""
+
+
+somador = Somador()
+
+for line in sys.stdin:
+    somador.sum_line(line)
+
